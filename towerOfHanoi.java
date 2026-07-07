091b6143f168f9db815711a8a9c9622a4b25f260
Indra Kumar <indravip3@gmail.com>
2026-07-07 10:36:21 +0530

    towerOfHanoi Problem

--- towerOfHanoi.java
+++ towerOfHanoi.java
@@ -1,3 +1,15 @@
 public class towerOfHanoi {
-    
+    public static void main(String args[]){
+        int n = 3;
+        toh(n, "A", "B", "C");
+    }
+    public static void toh(int n, String src, String help, String dest){
+        if(n == 1){
+            System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+            return;
+        }
+        toh(n-1, src, dest, help);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        toh(n-1, help, src, dest);
+    }
 }
