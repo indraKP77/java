7d4cbcf787fbe5155ee70713e7bfc3cb9fefdd36
Indra Kumar <indravip3@gmail.com>
2026-07-11 21:07:50 +0530

    uniqueSubSequences

--- subSequences.java
+++ subSequences.java
@@ -1,19 +1,27 @@
 import java.util.*;
+import java.util.HashSet;
 public class subSequences {
     public static void main(String args[]){
+        HashSet<String> set = new HashSet<>();
         Scanner sc = new Scanner(System.in);
         String str;
         System.out.println("Enter a string to find all subsequences");
         str = sc.nextLine();
-        findSubSequences(str, 0, "");
+        findSubSequences(str, 0, "", set);
         sc.close();
     }
-    public static void findSubSequences(String str, int idx, String newString){
+    public static void findSubSequences(String str, int idx, String newString, HashSet<String> set){
         if(idx == str.length()){
-            System.out.println(newString);
-            return;
+            if(set.contains(newString)){
+                return;
+            }
+            else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
         }
-        findSubSequences(str, idx+1, newString+str.charAt(idx));
-        findSubSequences(str, idx+1, newString);
+        findSubSequences(str, idx+1, newString+str.charAt(idx), set);
+        findSubSequences(str, idx+1, newString, set);
     }
 }
